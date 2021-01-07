7c78ebc1d763419b73087c8d6ffcb6301a046a12
KiniAjay <kiniajay2000@gmail.com>
2021-01-07 10:19:47 +0530

    poll single click func

--- maxo_project1/static/app.js
+++ maxo_project1/static/app.js
@@ -14,6 +14,7 @@ var handlePoll = function(member) {
   axios.post('http://localhost:5000/vote', {member: member})
   .then((data) => {
     console.log('data sent')
+    window.location.href = "http://127.0.0.1:5000/home";
   })
 }
 
