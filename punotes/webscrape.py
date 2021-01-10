35acc0cd223e52001cc6dfb4a87f1d55709e7355
bhatshreeram2001 <bhatshreeram2001@gmail.com>
2021-01-10 11:06:01 +0530

    punotes dropdown

--- punotes/webscrape.py
+++ punotes/webscrape.py
@@ -12,9 +12,8 @@ def notes():
     links_list=bSoup.find_all('a')
     for link in links_list:
         if 'href' in link.attrs:
-            values.append(str(link.attrs['href'])+'\n')        
+            values.append(str(link.attrs['href'])+'\n')
     return render_template('display.html', values=values)
 
 if __name__ == '__main__':
     app.run(debug = True)
-© 2021 GitHub, Inc.
